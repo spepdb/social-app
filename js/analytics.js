061b5a4c8c79a6d280050c6f13b02dccd84c7e57
spepdb <spepdb@mailfence.com>
2025-09-25 13:43:33 -0700

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,4 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-YOUR-TRACKING-ID'); // Replace with your Google Analytics ID
